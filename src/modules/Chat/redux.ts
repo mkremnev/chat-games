f597ac78bcba3ac22a0ebd7e8e938f5b1598b7f5
Maxim Kremnev <m.kremnev@netlinux.ru>
2021-03-25 21:11:57 +0300

    Created reducers

---
+++ src/modules/Chat/redux.ts
@@ -0,0 +1,48 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+export type Message = {
+	id: string;
+	from: string;
+	text: string;
+	createdAt: string;
+};
+
+export type chatState = {
+	skip: number;
+	messages: Message[];
+};
+
+export const initialState: chatState = {
+	skip: 0,
+	messages: [],
+};
+
+export const chatSlice = createSlice({
+	name: 'chat',
+	initialState,
+	reducers: {
+		changeSkip: (state, { payload }: PayloadAction<number>) => ({
+			...state,
+			skip: payload,
+		}),
+		addMessage: (
+			state,
+			{ payload }: PayloadAction<Message | Message[]>,
+		) => {
+			if (Array.isArray(payload)) {
+				return {
+					...state,
+					messages: [...state.messages, ...payload],
+				};
+			}
+			return {
+				...state,
+				messages: [...state.messages, payload],
+			};
+		},
+		sendMessage: (state, { payload }: PayloadAction<Message>) => ({
+			...state,
+			messages: [...state.messages, payload],
+		}),
+	},
+});
